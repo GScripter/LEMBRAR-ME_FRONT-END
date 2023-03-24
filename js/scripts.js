5bb4418d7af283003153e37c2a50b1e639215ee3
gabriel <santosgabriel6763@gmail.com>
2023-03-24 16:34:27 -0300

    Add Annotations Page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
-const SITE_CONTENT = "http://127.0.0.1:8000/"
 const platform_about = [...document.getElementsByClassName("about-platform")]
 const platform_values = [...document.getElementsByClassName("card")]
 const developer = [...document.getElementsByClassName("developer")]
 const privacy_policies = [...document.getElementsByClassName("privacy-policies")]
+// Cookies Alert
+if(!(localStorage.cookies)){
+    const cookies_alert = [...document.getElementsByClassName("cookies-alert")]
+    const btn_cookies = [...document.getElementsByClassName("btn-cookies")]
+    cookies_alert[0].style = "display: block;"
+    btn_cookies[0].addEventListener("click", () => {
+        localStorage.cookies = true
+        cookies_alert[0].style = "display: none";
+    })
+}
+
 // Theme
 const body = document.body
 const btn_theme = document.getElementById("btn-theme")
@@ -22,6 +32,11 @@ if(localStorage.theme){
 }
 
 // API
+const SITE_CONTENT = "http://127.0.0.1:8000/"
+const annotations = "http://127.0.0.1:8000/annotations/"
+const token_obtain = "http://127.0.0.1:8000/api/token/"
+const create_user = "http://127.0.0.1:8000/api/register/"
+
 fetch(SITE_CONTENT).then(response => response.json()).then(json => {
     // About Platform
     if(platform_about.length > 0){
@@ -44,3 +59,58 @@ fetch(SITE_CONTENT).then(response => response.json()).then(json => {
     }
 })
 
+// Login
+const login_form = document.getElementById("login-form")
+const alert_danger = document.getElementsByClassName("alert-danger")
+if(login_form){
+    login_form.addEventListener("submit", (evt) => {
+        evt.preventDefault()
+        fetch(token_obtain, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: evt.target.elements[0].value,
+                password: evt.target.elements[1].value,
+            }),
+        }).then(response => response.json()).then(json => {
+            localStorage.access = json.access
+            localStorage.refresh = json.refresh
+            if(json.access){
+                window.location = "annotations.html"
+            }else{
+                alert_danger[0].style = "display: block;"
+            }
+        })
+    })
+}
+
+
+// register
+const register_form = document.getElementById("register-form")
+if(register_form){
+    register_form.addEventListener("submit", (evt) => {
+        evt.preventDefault()
+        fetch(create_user, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: evt.target.elements[0].value,
+                email: evt.target.elements[1].value,
+                password: evt.target.elements[2].value,
+            }),
+        }).then(response => response.json()).then(json => console.log(json))
+    })
+}
+
+fetch(annotations, {
+    method: "GET",
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+localStorage.access
+    },
+}).then(response => response.json()).then(json => {
+})
